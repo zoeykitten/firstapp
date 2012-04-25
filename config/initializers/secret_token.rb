7027643bb04a19362c9e1e8fb9c6d0c951d749ed
Zoey Albini <zoe.albini@gmail.com>
2012-04-25 01:59:02 -0500

    Signed-off-by: Zoey Albini <zoe.albini@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f974a4e66d26a57f54462b9aade6dd0d30a7b98b146ce5aca856827afbab5463fbfc4b1825771e3c54a6bead3fe49abeaf3f5629184e798f64c3d136b8513b80'
